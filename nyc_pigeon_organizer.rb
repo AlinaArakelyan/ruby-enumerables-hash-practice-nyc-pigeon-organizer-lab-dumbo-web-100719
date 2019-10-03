c28f46c6cd8d3df4d9b299365a96d38180602dd6
Alina Arakelyan <alinaarakelyan90@gmail.com>
2019-10-03 05:20:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,24 +10,24 @@ end
 end
 
 
-def nyc_pigeon_organizer(data)
-  new_hash = {}
-  data.each do |key, value|
-    value.each do |new_value, names|
-      names.each do |name|
+# def nyc_pigeon_organizer(data)
+#   new_hash = {}
+#   data.each do |key, value|
+#     value.each do |new_value, names|
+#       names.each do |name|
        
-        if !new_hash[name]
-          new_hash[name] = {}
-        end
+#         if !new_hash[name]
+#           new_hash[name] = {}
+#         end
         
-        if !new_hash[name][key]
-          new_hash[name][key] = []
-       #binding.pry
-        end
-        new_hash[name][key] << new_value.to_s 
-        end
-      end
-    end
-  new_hash
+#         if !new_hash[name][key]
+#           new_hash[name][key] = []
+#       #binding.pry
+#         end
+#         new_hash[name][key] << new_value.to_s 
+#         end
+#       end
+#     end
+#   new_hash
   
-end
+# end
